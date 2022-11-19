401a46096815777561fe9ba90222e73e392135ab
benjaminrall <benr2508@aol.com>
2022-11-19 00:30:56 +0000

    constants (to remove)

---
+++ 2.0/constants.py
@@ -0,0 +1,27 @@
+BOT_TOKEN = "MTAxMjEyMDAwNTA1NzQ1NDEyMA.GK3NMU.EVsr33v5IID5tf-g8bMjkrafxPNDw0uCAgUDrA"
+VALORANT_ICON = "https://cdn.iconscout.com/icon/free/png-256/valorant-3251602-2724649.png"
+ADMINS = [312250768038297600]
+PINGS_CHANNEL = 1034961307381805107
+#ALL_USERS = [312250768038297600, 204316473882181633, 354758457167052811, 707654215849345057, 221264723512000512, 867176767022301204]
+ALL_USERS = [312250768038297600, 867176767022301204, 1036313168412155984]
+REJECTION_PENALTY = 90
+REASON_PENALTY = 30
+JOINING_TIME_LIMIT = 900
+NO_JOINING_PENALTY = 30
+NO_REASON_TIME_MULTIPLIER = 2
+
+TEMPLATE_DATA = {
+    "score": 0,
+    "ping_time": 0,
+    "penalties": 0,
+    "acceptances": 0,
+    "acceptance_time": 0,
+    "acceptances_missed": 0,
+    "no_reasons": 0,
+    "reasons": 0,
+    "votes_started": 0,
+    "valid_votes": 0,
+    "invalid_votes": 0,
+    "voted_against": 0,
+    "vote_penalties": 0
+}
